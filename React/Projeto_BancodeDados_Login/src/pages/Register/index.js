f2686e2fbdd2ea0c6666ded36d4b2bc3e774c960
AdeSSouza <adessouza@gmail.com>
2023-04-11 14:16:36 -0400

    Criando página register

--- React/Projeto_BancodeDados_Login/src/pages/Register/index.js
+++ React/Projeto_BancodeDados_Login/src/pages/Register/index.js
@@ -1,10 +1,54 @@
+import { useState } from 'react';
 
 
+import { Link } from 'react-router-dom';
 
-export default function Register(){
+export default function Home(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function handleLogin(e){
+      e.preventDefault();
+
+      if(email !== '' && password !== ''){
+        alert("Teste");
+      }else{
+        alert("Preencha todos os campos");
+      }
+    }
+
     return(
-      <div>
-        <h1>Página Register</h1>
+      <div className='home-container'>
+        <h1>Lista de tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil</span>
+
+        <form className='form' onSubmit={handleLogin}>
+          
+          <input 
+          type="text"
+          placeholder='Digite seu email...'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+          autoComplete={false}
+          type="password"
+          placeholder='******'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type='submit'>Acessar</button>
+
+        </form>
+
+        <Link className='button-link' to="/register" >
+          Não possui uma conta? Cadastre-se aqui
+        </Link>
+
       </div>
     )
   }
