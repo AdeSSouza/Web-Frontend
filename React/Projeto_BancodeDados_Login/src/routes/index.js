a308f949cb0a69f85a2a5ee23c3ce1ff3c7652d2
AdeSSouza <adessouza@gmail.com>
2023-04-11 15:56:21 -0400

    Controle de login do usuário

--- React/Projeto_BancodeDados_Login/src/routes/index.js
+++ React/Projeto_BancodeDados_Login/src/routes/index.js
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Register from '../pages/Register';
 import Admin from '../pages/Admin';
 
+import Private from './Private';
 
 
 function RoutesApp(){
@@ -11,7 +12,7 @@ function RoutesApp(){
         <Routes>
             <Route path='/' element={ <Home/> } />
             <Route path='/register' element={ <Register/> } />
-            <Route path='/admin' element={ <Admin/> } />
+            <Route path='/admin' element={ <Private> <Admin/> </Private> } />
         </Routes>
     )
 }
