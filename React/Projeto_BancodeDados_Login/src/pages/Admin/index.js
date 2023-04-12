36d746b6ee8b306b2184112a0357766eeee95b06
AdeSSouza <adessouza@gmail.com>
2023-04-12 09:32:08 -0400

    Registrando atividades no banco

--- React/Projeto_BancodeDados_Login/src/pages/Admin/index.js
+++ React/Projeto_BancodeDados_Login/src/pages/Admin/index.js
@@ -1,16 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './admin.css';
 
-import { auth } from '../../firebaseConnection';
+import { auth, db } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
+import {
+    addDoc,
+    collection,
+} from 'firebase/firestore';
+
+
 
 export default function Admin(){
     const [tarefaInput, setTarefaInput] = useState('');
+    const [user, setUser] = useState({})
 
-    function handleRegister(e){
+
+    useEffect(() => {
+        async function loadTarefas(){
+            const userDetail = localStorage.getItem("@detailUser")
+            setUser(JSON.parse(userDetail))
+        }
+
+        loadTarefas();
+    }, [])
+
+    async function handleRegister(e){
         e.preventDefault();
 
-        alert("Clicou!")
+        if(tarefaInput === ''){
+            alert("Digite sua tarefa...")
+            return
+        }
+
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user?.uid
+        })
+        .then(() => {
+            console.log("Tarefa Registrada")
+            setTarefaInput('')
+        })
+        .catch((error) => {
+            console.log("Erro ao registrar tarefa " + error)
+        })
     }
 
 
