fbe510ebaf94252253083867083bb1bbbd8e1c5c
AdeSSouza <adessouza@gmail.com>
2023-04-12 09:09:57 -0400

    Criando página de admin

--- React/Projeto_BancodeDados_Login/src/pages/Admin/index.js
+++ React/Projeto_BancodeDados_Login/src/pages/Admin/index.js
@@ -1,9 +1,49 @@
-import './admin.css'
+import { useState } from 'react';
+import './admin.css';
+
+import { auth } from '../../firebaseConnection';
+import { signOut } from 'firebase/auth';
 
 export default function Admin(){
+    const [tarefaInput, setTarefaInput] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault();
+
+        alert("Clicou!")
+    }
+
+
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
     return(
-        <div>
-            <h1>Página Admin</h1>
+        <div className='admin-container'>
+
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister}>
+                <textarea
+                placeholder='Digite sua tarefa...'
+                value={tarefaInput}
+                onChange={(e) => setTarefaInput(e.target.value)}
+                />
+
+                <button className='btn-register' type='submit'>Registrar Tarefa</button>
+            </form>
+
+            <article className='list'>
+                <p>Estudar JS e React hoje a noite</p>
+
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' onClick={handleLogout} >Sair</button>
+
         </div>
     )
 }
