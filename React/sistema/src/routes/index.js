88e796560c22a37cee28ad3cfb589fb49202bcd3
AdeSSouza <adessouza@gmail.com>
2023-04-24 08:40:08 -0400

    Pagina clientes

--- React/sistema/src/routes/index.js
+++ React/sistema/src/routes/index.js
@@ -4,6 +4,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Customers from "../pages/Custumers";
 
 import Private from "./Private";
 
@@ -12,8 +13,10 @@ function RoutesApp(){
         <Routes>
             <Route path="/" element={ <SignIn/> } />
             <Route path="/register" element={ <SignUp/> } />
+
             <Route path="/dashboard" element={ <Private> <Dashboard/> </Private>} />
             <Route path="/profile" element={ <Private><Profile/></Private>} />
+            <Route path="/customers" element={<Private><Customers/></Private>} />
         </Routes>
     )
 }
