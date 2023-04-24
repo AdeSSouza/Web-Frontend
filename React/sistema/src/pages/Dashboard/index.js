dd7c6ff92936bce9b2de6d6272b02bf0a0687397
AdeSSouza <adessouza@gmail.com>
2023-04-24 09:26:42 -0400

    Criando página dashboard

--- React/sistema/src/pages/Dashboard/index.js
+++ React/sistema/src/pages/Dashboard/index.js
@@ -1,6 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import './dashboard.css';
 
 export default function Dashboard(){
     const { logout } = useContext(AuthContext);
@@ -12,6 +18,51 @@ export default function Dashboard(){
     return(
         <div>
             <Header/>
+
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#fff" size={25} />
+                        Novo chamado
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-aria-label="Cliente">Mercado Esquina</td>
+                                <td data-aria-label="Assunto">Pão</td>
+                                <td data-aria-label="Status">Aberto</td>
+                                <td data-aria-label="Cadastrado">2023</td>
+                                <td data-aria-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch color="#fff" size={17} />
+                                    </button>
+
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }} >
+                                        <FiEdit2 color="#fff" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </>
+
+            </div>
+
             <button onClick={handleLogout}  >Sair da conta</button>
         </div>
     )
